6b375acd13df4f875e66cd36372aea9dc68b7577
Focus4Study <f4studies@gmail.com>
2024-05-11 17:01:12 +0600

    style for Add blog page added

--- src/Pages/AddBlog/AddBlog.jsx
+++ src/Pages/AddBlog/AddBlog.jsx
@@ -3,7 +3,53 @@
 const AddBlog = () => {
     return (
         <div>
-            <p>This is the add blog page</p>
+            <section className="p-6 dark:bg-gray-100 dark:text-gray-900" style={{ backgroundImage: 'url(https://i.ibb.co/w7PyjsY/5357377-Internet-go.jpg)', backgroundPosition: 'center' }}>
+                <form className="container flex flex-col mx-auto space-y-12  mb-20">
+                    <div className="grid grid-cols-2 gap-10 mt-20">
+
+                        <div>
+                            <label htmlFor="image" className="text-xl font-bold  mr-5">Image Url</label>
+                            <input name="image" id="image" type="text" placeholder="Image Url" className="input input-bordered w-full  focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                        </div>
+
+
+
+                        <div>
+                            <label htmlFor="title" className="text-xl font-bold mr-5">Title</label>
+                            <input name="title" id="title" type="text" placeholder="Title" className="input input-bordered w-full  focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                        </div>
+
+                        <div>
+                            <label htmlFor="User Name" className="text-xl font-bold mr-5">User Name</label>
+                            <input name="name" id="user_name" type="text" placeholder="User Name" className="input input-bordered w-full  focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                        </div>
+
+                        <div>
+                            <label htmlFor="User Email" className="text-xl font-bold mr-5">User Email</label>
+                            <input name="email" id="user_email" type="email" placeholder="User Email" className="input input-bordered w-full  focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                        </div>
+
+
+
+                        <div>
+                            <label htmlFor="short description" className="text-xl font-bold mr-5">Short Description</label>
+                            <textarea name="short_description" id="" cols="50" rows="10" type="text" placeholder="Short Description" className="input input-bordered h-28 w-full  focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" ></textarea>
+
+                        </div>
+
+                        <div>
+                            <label htmlFor="Long description" className="text-xl font-bold mr-5">Long Description</label>
+                            <textarea name="long_description" id="" cols="50" rows="10" type="text" placeholder="Short Description" className="input input-bordered w-full  h-28 focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" ></textarea>
+                        </div>
+
+                        <div>
+                            <label htmlFor="Category_Name" className="text-xl font-bold mr-5">Category Name</label>
+                            <input name="category_Name" id="category_Name" type="text" placeholder="Category Name" className="input input-bordered w-full  focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
+                        </div>
+                    </div>
+                    <input type="submit" className="btn text-white font-bold bg-[#D04848]" value="Submit" />
+                </form>
+            </section>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,13 +6,13 @@ const Home = () => {
         <div className="container mx-auto space-y-10">
             <Carousel autoplay className="custom-slider rounded-lg max-h-36 md:max-h-52 lg:max-h-max">
                 <div>
-                    <img src="https://i.ibb.co/C1p3wT9/52257.jpg" />
+                    <img className='w-full' src="https://i.ibb.co/C1p3wT9/52257.jpg" />
                 </div>
                 <div>
-                    <img src="https://i.ibb.co/tJGnRqx/9220705.jpg" />
+                    <img className='w-full' src="https://i.ibb.co/tJGnRqx/9220705.jpg" />
                 </div>
                 <div>
-                    <img src="https://i.ibb.co/3NpZqrb/47465.jpg" />
+                    <img className='w-full' src="https://i.ibb.co/3NpZqrb/47465.jpg" />
                 </div>
             </Carousel>
 
